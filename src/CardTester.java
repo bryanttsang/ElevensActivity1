3375f3495815f1dad5fd0bb766981649c95b6705
BT_1N3_21 <wtsang1931@bths.edu>
2019-02-14 14:28:19 -0500

    finished

--- src/CardTester.java
+++ src/CardTester.java
@@ -2,4 +2,17 @@
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+
+    public static void main(String args[])
+    {
+        Card card1 = new Card("A", "spade", 1);
+        Card card2 = new Card("A", "spade", 1);
+        Card card3 = new Card("J", "spade", 10);
+
+        System.out.println(card1.matches(card2));
+        System.out.println(card2.matches(card3));
+        System.out.println(card3.matches(card1));
+    }
+
+
 }
